6b909c9e9b1236a05721df5d8baa0a04795bd269
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-22 11:07:38 +0300

    Styling homepage logo

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,31 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from "./routes/HomePage/HomePage"
 import DashboardPage from './routes/DashboardPage/DashboardPage';
 import ChatPage from './routes/ChatPage/ChatPage'
+import RootLayout from './layouts/RootLayout/RootLayout';
+import DashboardLayout from './layouts/DashboardLayout/DashboardLayout';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage/>,
-  },
-  {
-    path: "/dashboard",
-    
-    children: [{path:"/dashboard", element: <DashboardPage />,}, {path:"/dashboard/chats/:id", element:<ChatPage />}
-
-    ],
+    element: <RootLayout/>,
+    children:[
+      {
+        path:"/",
+        element: <HomePage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children:[
+          {
+            path:"/dashboard",
+            element:<DashboardPage />
+          },
+          {
+            path:"/dashboard/chats/:id",
+            element:<ChatPage />,
+          }
+        ]
+      }
+    ]
   },
 ]);
 
