cb8837a52525a202cfd0fd627c2d771aa75673c4
Anthony Ngonde <anto.ngonde@gmail.com>
2024-10-29 10:16:07 +0300

    Styling the chat page

--- src/routes/ChatPage/ChatPage.jsx
+++ src/routes/ChatPage/ChatPage.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './ChatPage.css'
 
 const ChatPage = () => {
+
+  const endRef = useRef(null)
+
+useEffect(() =>{
+  endRef.current.scrollIntoView({behavior:"smooth"});
+},[]);
+
+
   return (
-    <div>ChatPage</div>
+    <div className='chatPage'>
+      <div className="wrapper">
+        <div className='chat'>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam cum vero molestias incidunt et consectetur magni eaque, saepe animi vel molestiae iste. Tenetur aut ab ex eveniet vitae vel laboriosam.</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div ref={endRef}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
