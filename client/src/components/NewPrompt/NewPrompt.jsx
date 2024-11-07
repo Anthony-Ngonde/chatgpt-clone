13f3646c64292636956a70d75a4a76100ffcc108
Anthony Ngonde <anto.ngonde@gmail.com>
2024-11-07 10:54:53 +0300

    Got my first gen ai response

--- client/src/components/NewPrompt/NewPrompt.jsx
+++ client/src/components/NewPrompt/NewPrompt.jsx
@@ -7,6 +7,8 @@ import model from "../../lib/gemini"
 
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState("")
   
   const [img, setImg] = useState({
     isLoading:false,
@@ -20,14 +22,22 @@ const NewPrompt = () => {
     endRef.current.scrollIntoView({behavior:"smooth"});
   },[]);
 
-  const add = async () => {
-    const prompt = "Write a story about a magic backpack.";
+  const add = async (text) => {
+    // const prompt = "Write a story about a magic backpack.";
+    setQuestion(text)
 
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(text);
     const response = await result.response;
-    const text = response.text()
-    console.log(text);
+    setAnswer(response.text())
+  }
 
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+
+    const text = e.target.text.value;
+    if (!text) return;
+
+    add(text);
   }
   
 
@@ -44,12 +54,13 @@ const NewPrompt = () => {
         transformation={[{width:380}]}
       />
     )}
-    <button onClick={add}>TEST AI</button>
+    {question && <div className='message user'>{question}</div>}
+    {answer && <div className='message'>{answer}</div>}
     <div className="endChat" ref={endRef}></div>
-        <form className="newForm">
+        <form className="newForm" onSubmit={handleSubmit}>
             <Upload setImg={setImg} />
             <input id="file" type="file" multiple={false} hidden/>
-            <input type="text" placeholder='Ask anything...' />
+            <input type="text" name='text' placeholder='Ask anything...' />
             <button>
                 <img src="/arrow.png" alt="" />
             </button>
